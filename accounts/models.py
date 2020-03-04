f50db8e4358995c44384faafa4bb7df8c1b552f1
Nikhil Sahu <Nikhilsahu.in@example.com>
2020-03-04 12:01:34 +0530

    auto ganerate token

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,13 @@ from django.conf import settings
 
 from django.contrib.auth.models import Group
 
+#
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+#
+
 
 class UserProfile(BaseUserManager):
     def create_user(self, email, username, password=None):
@@ -49,6 +56,15 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+#auto token ganerated
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+#
 
 class ProfileStatus(models.Model):
     user_profile = models.ForeignKey(
